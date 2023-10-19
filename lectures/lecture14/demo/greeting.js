da6fceca6b2ce83733632df91616ef6b1418f124
Isabella <Ihernan2@unca.edu>
2023-10-18 21:30:22 -0400

    hw5

---
+++ lectures/lecture14/demo/greeting.js
@@ -0,0 +1,15 @@
+function sayHello() {
+    // your code here...
+    document.querySelector("#message").innerHTML="<strong>oh hey.</strong>";
+    document.querySelector("#message").style.color= "green";
+
+
+}
+
+
+function sayGoodbye() {
+    // your code here...
+    document.querySelector("#message").innerHTML='buh buy.'
+    document.querySelector("#message").style.color= "grey";
+
+}
