c3a6a04c7086df0b03ea25867943c8007857fb9f
Isabella <Ihernan2@unca.edu>
2023-12-08 17:12:21 -0500

    exam

---
+++ javascript-final-exam/exercise01/main.js
@@ -0,0 +1,12 @@
+function makeBlue() {
+ console.log('Change background to blue');
+document.querySelector('#s1').style.backgroundColor ='lightblue';
+};
+
+function goodbye() {
+    document.getElementById("message").innerHTML = "Goodbye!" ;
+};
+
+function showDog() {
+document.querySelector("#animal1") .src = "images/dog.jpg"
+};
---
+++ quiz03/quiz03/main.js
@@ -0,0 +1,23 @@
+function color1() {
+    
+}
+
+function color2() {
+///document.querySelector("#square2").style.backgroundColor = "blue";
+}
+
+function color3() {
+    // TODO
+}
+
+function color4() {
+    // TODO
+}
+
+function color5() {
+    // TODO
+}
+
+function color6() {
+    // TODO
+}
