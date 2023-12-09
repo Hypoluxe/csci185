d2369ee8e7e8f7d35615b8b34213d46942da08a6
Isabella <Ihernan2@unca.edu>
2023-12-08 22:00:47 -0500

    carousel

--- homework/hw06/carousel.js
+++ homework/hw06/carousel.js
@@ -12,33 +12,25 @@ const photos = [
 ];
 let idx = 0;
 
-
-/* This function should:
-    (1) display the new image inside of the .current-photo container, and 
-    (2) update the caption inside of the .caption paragraph
-*/
 function showImage() {
-    console.log('Show image');
+    document.querySelector('#photo') .src = photos [idx];
+    document.querySelector('.caption').innerHTML =  `Photo ${idx + 1} of 10`;
 }
 
-
-/* This function should set the idx variable 
-   to one greater than the current value of idx, 
-   and then invoke the showImage() function.
-   If the idx gets to one less than the length of 
-   the array, set idx to 0.
-*/
 function forward() {
-    console.log('forward');
+    if (idx ==9) {
+        idx = 0;
+    } else{
+    idx = idx +1;
+    }
+    showImage();
 }
 
-
-/* This function should set the idx variable 
-   to one less than the current value of idx, 
-   and then invoke the showImage() function.
-   If the idx gets to the beginning, set idx to
-   one less than the length of the array.
-*/
 function back() {
-    console.log('back');
+    if (idx ==0) {
+        idx=9;
+    } else {
+            idx= idx -1;
+}
+    showImage();
 }
